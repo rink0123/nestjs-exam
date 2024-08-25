aa1dce82a1719fd5e9bf201276d56a90c69ab0e4
kangminsung <rink0123@naver.com>
2024-08-26 02:28:39 +0900

    code review: user dto: nickname validation strict
    nickname empty 여부만 따지기에 여타 다른 사이트와 같이 닉네임 유효성 검사 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,17 +1,38 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsStrongPassword,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail(
+    {},
+    {
+      message: '유효하지 않은 이메일 형식입니다.',
+    },
+  )
   id: string;
 
-  @IsStrongPassword({
-    minLength: 8,
-    minUppercase: 0,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minUppercase: 0,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        '비밀번호는 최소 8자 이상이어야 하며, 숫자 1개와 특수문자 1개 이상을 포함해야 합니다.',
+    },
+  )
   password: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: '닉네임은 비워둘 수 없습니다.',
+  })
+  @Matches(/^[a-zA-Z0-9가-힣]+$/, {
+    message: '닉네임은 영문자, 숫자, 한글만 포함할 수 있습니다.',
+  })
   nickname: string;
 }
