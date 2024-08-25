0f173265300378a31025030f9abfa2588cd3e202
kangminsung <rink0123@naver.com>
2024-08-26 01:55:30 +0900

    code review: dupl user repository
    before: controller>service>repository>repository
    after: controller>service>repository

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { UserRepository } from './user.repository';
 import { User } from 'src/database/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService],
 })
 export class UserModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UserRepository } from './user.repository';
+import { User } from 'src/database/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
-    return await this.userRepository.create(createUserDto);
+    await this.userRepository
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values(createUserDto)
+      .returning('*')
+      .execute();
+
+    return {};
   }
 }
